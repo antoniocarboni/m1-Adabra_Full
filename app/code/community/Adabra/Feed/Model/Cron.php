c601661bf8cc40060a6767b45ce65bec116b0e79
Riccardo Tempesta <info@riccardotempesta.com>
2017-06-16 10:32:17 +0200

    FIX AOE_Scheduler issue + New logo

--- app/code/community/Adabra/Feed/Model/Cron.php
+++ app/code/community/Adabra/Feed/Model/Cron.php
@@ -20,14 +20,14 @@
 
 class Adabra_Feed_Model_Cron
 {
-    public function buildNext(Varien_Event_Observer $event)
+    public function buildNext($event)
     {
         if (Mage::helper('adabra_feed')->isCronEnabled()) {
             Mage::getSingleton('adabra_feed/feed')->exportNext();
         }
     }
 
-    public function rebuildFeeds(Varien_Event_Observer $event)
+    public function rebuildFeeds($event)
     {
         if (Mage::helper('adabra_feed')->isCronEnabled()) {
             $hr = intval(Mage::getSingleton('core/date')->date('H'));
